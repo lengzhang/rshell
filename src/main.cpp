68c75e918205a99417f527ebf9af92a7eea70406
Leng <lengzhang@live.com>
2016-05-13 01:50:30 -0700

    single cmd succeed

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <vector>
+#include <array>
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,19 +11,25 @@
 #include <sys/wait.h>
 
 int EXEC (char * args[]);
-int do_EXEC (char * args[]);
+int do_EXEC (char * args);
 
-int main ( int argc, char * argv[] )
+int main (int argc, char * argv[])
 {
 	std :: string cmdStr;
-	std :: cout << "$ ";
-	std :: getline (std :: cin, cmdStr);
+	do
+	{
+		std :: cout << "$ ";
+		std :: getline (std :: cin, cmdStr);
 
-	char * cmdCStr = new char[cmdStr.length () + 1];
-	std :: strcpy (cmdCStr, cmdStr.c_str());
-	cmdCStr[cmdStr.length ()] = '\0';
-	printf("%s\n", cmdCStr);
-
+		if (!cmdStr.empty())
+		{
+			char * cmdCStr = new char[cmdStr.length () + 1];
+			std :: strcpy (cmdCStr, cmdStr.c_str());
+			cmdCStr[cmdStr.length ()] = '\0';
+			printf ("%s\n", cmdCStr);
+			do_EXEC (cmdCStr);
+		}
+	} while (!cmdStr.empty());
 	return 0;
 }
 
@@ -55,4 +63,33 @@ int EXEC (char * args[])
 	}
 
 	return status;
+}
+
+int do_EXEC (char * args)
+{
+	char * tmpARGS = args;
+	std :: vector <char *> args_vector;
+	char * pch = strtok (tmpARGS, " ");
+	while (pch != NULL)
+	{
+		args_vector.push_back (pch);
+		pch = strtok (NULL, " ");
+	}
+	args_vector.push_back (0);
+	int args_size = args_vector.size ();
+	char * args_result[10];
+	for (int i = 0; i < args_size; i++)
+	{
+		args_result[i] = args_vector[i];
+	}
+	
+	EXEC(args_result);
+
+
+
+
+
+
+
+	return 0;
 }
